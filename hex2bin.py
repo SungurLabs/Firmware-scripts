93be96e7cbb35665488f12c8801a6d5e953d3d6f
Agha-Shadi <29219050+Agha-Shadi@users.noreply.github.com>
2023-02-06 00:10:43 +0100

    Update hex2bin.py
    didn't need to read the whole file at the beginning.
    made it a bit more self descriptive.
    binary couldn't be concatenated with "data_bin" string.

--- hex2bin.py
+++ hex2bin.py
@@ -2,16 +2,13 @@
 
 import sys, struct
 
-data_bin = ''
+data_bin = bytearray()
 with open(sys.argv[1]) as hexdump:
-	data = hexdump.read()
-	data = data.split("\n")
-	for i in data:
-		data_hex = i[10:57].split(' ')
-		for j in data_hex:
-			data_bin += struct.pack("B", int(j, 16))
+        for line in hexdump:
+                data_hex= line[10:57].split(" ")
+                for i in data_hex:
+                        data_bin += struct.pack("B", int(i, 16))
 
 binary_file = open(sys.argv[2], "wb")
 binary_file.write(data_bin)
 binary_file.close()
-
